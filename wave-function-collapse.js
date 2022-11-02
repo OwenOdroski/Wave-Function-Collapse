ac3719a0b6ae8bb5f15d718f73c9f7fd09f6ac97
OwenOdroski <116469702+OwenOdroski@users.noreply.github.com>
2022-11-02 12:51:52 -0400

    Update wave-function-collapse.js

--- wave-function-collapse.js
+++ wave-function-collapse.js
@@ -1,5 +1,8 @@
 var size = 14
-var c = document.querySelector('canvas')
+var c = document.createElement('canvas')
+c.setAttribute('id', 'wfc-canvas')
+document.body.appendChild(c)
+
 var ctx = c.getContext("2d")
 var source = [
     [
@@ -68,11 +71,6 @@ var tiles = [
     [],
     []
 ]
-var types = [
-    [],
-    [],
-    []
-]
 var sides = [
     [1, 3, 4, 6, 8, 9, 10],
     [1, 2, 5, 6, 7, 9, 10],
@@ -114,67 +112,80 @@ function createTile(x, y, num) {
             }
         }
     }
-    types[0].push(x)
-    types[1].push(y)
-    types[2].push(num)
 }
 
 function createBuildings(x, y) {
     ctx.fillStyle = '#964B00'
     ctx.fillRect(x, y, 10, 10)
 }
+var wfc = {
+    start: function() {
+        let top = []
+        let lastY = []
+        let last = 6
 
-function start() {
-    let top = []
-    let newTop = []
-    let lastY = []
-    let last = 6
-
-    for(let i = 0; i < size; i++) {
-        let rand = sides[1][Math.floor(Math.random() * sides[0].length)]
-        createTile(i * 30, -30, rand)
-        top.push(rand)
-    }
-    for(let i = 0; i < size; i++) {
-        let rand = sides[1][Math.floor(Math.random() * sides[0].length)]
-        createTile(-30, (i * 30), rand)
-        lastY.push(rand)
-    }
-    for(let u = 0; u < size; u++) {
         for(let i = 0; i < size; i++) {
-            let type = {
-                top: false,
-                left: false
-            }
+            let rand = sides[1][Math.floor(Math.random() * sides[0].length)]
+            createTile(i * 30, -30, rand)
+            top.push(rand)
+        }
+        for(let i = 0; i < size; i++) {
+            let rand = sides[1][Math.floor(Math.random() * sides[0].length)]
+            createTile(-30, (i * 30), rand)
+            lastY.push(rand)
+        }
+        for(let u = 0; u < size; u++) {
+            for(let i = 0; i < size; i++) {
+                let type = {
+                    top: false,
+                    left: false
+                }
 
-            if(bl[0].includes(top[i])) {
-                type.top = true
-            }
-            if(i == 0) {
-                last = lastY[u]
-            }
-            if(bl[1].includes(last)) {
-                type.left = true
-            }
+                if(bl[0].includes(top[i])) {
+                    type.top = true
+                }
+                if(i == 0) {
+                    last = lastY[u]
+                }
+                if(bl[1].includes(last)) {
+                    type.left = true
+                }
 
-            if(type.top && type.left) {
-                let rand = tlb[2][Math.floor(Math.random() * tlb[2].length)]
-                createTile(i * 30, u * 30, rand)
-                last = rand
-            } else if(type.top && !type.left) {
-                let rand = tlb[0][Math.floor(Math.random() * tlb[0].length)]
-                createTile(i * 30, u * 30, rand)
-                last = rand
-            } else if(!type.top && type.left) {
-                let rand = tlb[1][Math.floor(Math.random() * tlb[1].length)]
-                createTile(i * 30, u * 30, rand)
-                last = rand
-            } else {
-                let rand = tlb[3][Math.floor(Math.random() * tlb[3].length)]
-                createTile(i * 30, u * 30, rand)
-                last = rand
+                if(type.top && type.left) {
+                    let rand = tlb[2][Math.floor(Math.random() * tlb[2].length)]
+                    createTile(i * 30, u * 30, rand)
+                    last = rand
+                } else if(type.top && !type.left) {
+                    let rand = tlb[0][Math.floor(Math.random() * tlb[0].length)]
+                    createTile(i * 30, u * 30, rand)
+                    last = rand
+                } else if(!type.top && type.left) {
+                    let rand = tlb[1][Math.floor(Math.random() * tlb[1].length)]
+                    createTile(i * 30, u * 30, rand)
+                    last = rand
+                } else {
+                    let rand = tlb[3][Math.floor(Math.random() * tlb[3].length)]
+                    createTile(i * 30, u * 30, rand)
+                    last = rand
+                }
             }
         }
+    },
+    size: function(value) {
+        size = value
+        c.width = (size * 3) * 10
+        c.height = (size * 3) * 10
+    },
+    canvas: function(name) {
+        c.setAttribute('id', name)
+    },
+    tiles: function() {
+        var obj = {
+            x: tiles[0],
+            y: tiles[1],
+            tile: tiles[2]
+        }
+
+        return obj
     }
 }
-start()
